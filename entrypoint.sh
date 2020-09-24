92566f90481402bfbabf687345c8bd0e644ce8fb
Sam Albon <sam.m.albon@outlook.com>
2020-09-24 11:14:54 -0400

    Removed references to APP_DIR

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,6 @@ dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "======================================="
 echo "$dt. Starting container..."
 
-# move on to the work directory
-cd $APP_DIR
-echo "App dir: $APP_DIR"
-
-if [ -f bower.json ]; then
-  bower install --allow-root & wait
-fi
-
-if [ -f yarn.lock ]; then
-  yarn install & wait
-elif [ -f package-lock.json ]; then
-  npm install & wait
-else
-  yarn install & wait
-fi
-
 #echo "Starting chrome for testing"
 xvfb-run --server-args='-screen 0, 1024x768x16' google-chrome -start-maximized > /dev/null &
 
